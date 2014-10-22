ab299dc2614feadee1f4485c65a7dfbe0a0d9d75
Nick Jones <nick.jones@datacentred.co.uk>
2014-10-22 21:25:39 +0100

    Add path attributes to execs

--- manifests/client.pp
+++ manifests/client.pp
@@ -24,10 +24,12 @@ define ceph::client (
 
   exec { "echo -n '[${user}]\\n\\tkey = ' > ${keyring}":
     unless => "grep ${user} ${keyring}",
+    path   => '/bin:/usr/bin',
   } ~>
 
   exec { "ssh -F /home/${ceph::params::deploy_user}/.ssh/config -i /home/${ceph::params::deploy_user}/.ssh/id_rsa ${ceph::params::deploy_user}@${ceph::params::mon_initial_member} sudo \"ceph auth get-or-create-key ${user} ${perms}\" >> ${keyring}":
     refreshonly => true,
+    path        => '/bin:/usr/bin',
   }
 
   # Exec is inherited from ::ceph, which plays havoc with concat
@@ -38,7 +40,7 @@ define ceph::client (
   }
 
   concat::fragment { "ceph.${user}.conf":
-    target  => "/etc/ceph/ceph.conf",
+    target  => '/etc/ceph/ceph.conf',
     content => template('ceph/ceph.client.conf.erb'),
     order   => '20',
   }
